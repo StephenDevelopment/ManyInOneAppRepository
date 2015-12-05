5eb440b6e5e0ccf3f595c121adc91f2288f74e32
StephenEve <stephenraj@evapps.in>
2015-12-05 18:11:32 +0530

    Added comment in main activity as Me too

--- app/src/main/java/com/github/admin/manyinoneapp/MainActivity.java
+++ app/src/main/java/com/github/admin/manyinoneapp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Me too
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
